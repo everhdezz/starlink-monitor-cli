c3708ccaad11b219da33f04b4609f4c6fafd24cb
“Ever <everhdezz@gmail.com>
2024-11-10 01:08:32 -0600

    v1.0.2: updated the ping devices command

--- src/actions/devices/pingDevices.js
+++ src/actions/devices/pingDevices.js
@@ -11,27 +11,32 @@ const getGrpcDeviceInfo = require('../../helpers/getGrpcDeviceInfo');
 module.exports = async ({}) => {
     const devices = configManager.get('devices');
 
-    devices.map(pingDevice);
+    console.log(clc.blue(`Pinging ${devices.length} devices...`));
+
+    await Promise.all(
+        devices.map(async (device) =>
+            await pingDevice(device)
+        )
+    );
+
+    console.log(clc.blue('Pinging devices completed.'));
 
     process.exit(1);
 }
 
 async function pingDevice({ token, server }) {
     try {
+        console.log(`Pinging device ${server.host}:${server.port}...`);
+
         const deviceInfo = getGrpcDeviceInfo(server.host, server.port);
 
-        await axios
-            .get(`api/device`, {
-                params: { deviceInfo },
+        return await axios
+            .post(`api/device/ping`, deviceInfo, {
                 headers: {
                     Authorization: `Bearer ${token}`,
-                },
+                }
             });
     } catch (error) {
         console.log(clc.red(error));
     }
-}
-
-function getDeviceInfo() {
-    getGrpcDeviceInfo
 }
